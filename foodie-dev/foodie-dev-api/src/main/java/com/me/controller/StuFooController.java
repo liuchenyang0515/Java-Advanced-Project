d8e8601e2fbc00f730c55ba76cbe2468c717c6df
liuchenyang0515 <897418643@qq.com>
2021-04-10 13:31:07 +0800

    2-22 基于通用Mapper基于Rest编写api接口2

--- foodie-dev/foodie-dev-api/src/main/java/com/me/controller/StuFooController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/me/controller/StuFooController.java
@@ -4,6 +4,7 @@ import com.me.pojo.Stu;
 import com.me.service.StuService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -16,5 +17,24 @@ public class StuFooController {
     @GetMapping("/getStu/{id}")
     public Object getStu(@PathVariable("id") int id) {
         return stuService.getStuInfo(id);
+    }
+
+
+    @PostMapping("/saveStu")
+    public Object saveStu() {
+        stuService.saveStu();
+        return "OK";
+    }
+
+    @PostMapping("/updateStu")
+    public Object updateStu(int id) {
+        stuService.updateStu(id);
+        return "OK";
+    }
+
+    @PostMapping("/deleteStu")
+    public Object deleteStu(int id) {
+        stuService.deleteStu(id);
+        return "OK";
     }
 }
