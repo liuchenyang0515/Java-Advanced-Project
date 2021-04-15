27a3c548cd8ce24698b7b9e7ab6ec9595c337291
liuchenyang0515 <897418643@qq.com>
2021-04-15 10:28:24 +0800

    3-2 用户注册-判断用户名是否存在

---
+++ foodie-dev/foodie-dev-service/src/main/java/com/me/service/UserService.java
@@ -0,0 +1,9 @@
+package com.me.service;
+
+public interface UserService {
+
+    /**
+     * 判断用户名是否存在
+     */
+    public boolean queryUsernameIsExist(String username);
+}
---
+++ foodie-dev/foodie-dev-service/src/main/java/com/me/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.me.service.impl;
+
+
+import com.me.mapper.UsersMapper;
+import com.me.pojo.Users;
+import com.me.service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+import javax.annotation.Resource;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Resource
+    public UsersMapper usersMapper;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public boolean queryUsernameIsExist(String username) {
+
+        Example userExample = new Example(Users.class);
+        Example.Criteria userCriteria = userExample.createCriteria();
+
+        userCriteria.andEqualTo("username", username);
+
+        Users result = usersMapper.selectOneByExample(userExample);
+
+        return result == null ? false : true;
+    }
+}
