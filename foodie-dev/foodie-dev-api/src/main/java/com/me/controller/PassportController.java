bef49a46224fd8e4f1ffb9017c92b2f869376804
liuchenyang0515 <897418643@qq.com>
2021-04-15 16:42:45 +0800

    3-3 自定义响应数据结构--统一返回对象模型ModelJSONResult.java的添加

--- foodie-dev/foodie-dev-api/src/main/java/com/me/controller/PassportController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/me/controller/PassportController.java
@@ -1,7 +1,9 @@
 package com.me.controller;
 
 import com.me.service.UserService;
+import com.me.utils.ModelJSONResult;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,21 +19,21 @@ public class PassportController {
     private UserService userService;
 
     @GetMapping("/usernameIsExist")
-    public int usernameIsExist(@RequestParam String username) {
+    public ModelJSONResult usernameIsExist(@RequestParam String username) {
 
         // 1. 判断用户名不能为空
         if (StringUtils.isBlank(username)) {
-            return 500;
+            return ModelJSONResult.errorMsg("用户名不能为空");
         }
 
         // 2. 查找注册的用户名是否存在
         boolean isExist = userService.queryUsernameIsExist(username);
         if (isExist) {
-            return 500;
+            return ModelJSONResult.errorMsg("用户名已经存在");
         }
 
         // 3. 请求成功，用户名没有重复
-        return 200;
+        return ModelJSONResult.ok();
     }
 
 
--- foodie-dev/foodie-dev-api/src/test/java/com/test/TransTest.java
+++ foodie-dev/foodie-dev-api/src/test/java/com/test/TransTest.java
@@ -10,9 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = Application.class)
 public class TransTest {
 
     @Resource
