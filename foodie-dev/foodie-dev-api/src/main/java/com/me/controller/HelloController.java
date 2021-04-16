79672636f90949d88f30e8c7f62a9afcdc85ada0
liuchenyang0515 <897418643@qq.com>
2021-04-16 21:04:04 +0800

    3-12 回顾cookie与session--测试session

--- foodie-dev/foodie-dev-api/src/main/java/com/me/controller/HelloController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/me/controller/HelloController.java
@@ -4,11 +4,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @RestController
 @ApiIgnore
 public class HelloController {
     @GetMapping("/hello")
     public Object hello() {
         return "Hello World~";
+    }
+
+    @GetMapping("/setSession")
+    public Object setSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.setAttribute("userInfo", "new user");
+        // 设置过期时间，0代表永不过期
+        session.setMaxInactiveInterval(3600);
+        session.getAttribute("userInfo");
+//        session.removeAttribute("userInfo");
+        return "ok";
     }
 }
