427e2cd158d5647dcc143f0d2c11f8e87859760b
liuchenyang0515 <897418643@qq.com>
2021-05-06 17:06:16 +0800

    5-6 购物车-删除商品业务讲解--预留代码到讲解redis部分再写

--- foodie-dev/foodie-dev-api/src/main/java/com/me/controller/ShopcatController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/me/controller/ShopcatController.java
@@ -30,4 +30,16 @@ public class ShopcatController {
         System.out.println(shopcartBO);
         return ModelJSONResult.ok();
     }
+
+
+    @ApiOperation(value = "从购物车中删除商品", notes = "从购物车中删除商品", httpMethod = "POST")
+    @PostMapping("/del")
+    public ModelJSONResult del(@RequestParam String userId, @RequestBody String itemSpecId,
+                               HttpServletRequest request, HttpServletResponse response) {
+        if (StringUtils.isBlank(userId) || StringUtils.isBlank(itemSpecId)) {
+            return ModelJSONResult.errorMsg("参数不能为空");
+        }
+        // TODO 用户在页面删除购物车中的商品数据，如果此时用户已经登录，则需要同步删除后端购物车中的数据
+        return ModelJSONResult.ok();
+    }
 }
